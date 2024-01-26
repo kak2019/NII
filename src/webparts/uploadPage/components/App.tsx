ddc30b57122c62e8dc92167fe7ba73efd449aed5
Flynt Gao <flynt.gao@consultant.udtrucks.com>
2024-01-26 15:17:13 +0800

    change upload parse function

--- src/webparts/uploadPage/components/App.tsx
+++ src/webparts/uploadPage/components/App.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable dot-notation */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable */
 import * as React from "react";
 import { memo } from "react";
 //import { read, utils, SSF }  as XLSX from 'xlsx';
@@ -17,6 +18,7 @@ import { UploadOutlined } from '@ant-design/icons';
 // import { AadHttpClient, HttpClientResponse } from '@microsoft/sp-http';
 import { useEffect } from "react";
 import { mytoken } from "../UploadPageWebPart";
+import * as moment from "moment";
 // import * as moment from "moment";
 // import helpers from "../../../config/Helpers";
 // 定义 Excel 文件中数据的类型
@@ -92,6 +94,45 @@ const getSubTableData = (arr: Array<{ [key in string]: any }>) => {
     return res
 }
 
+// 获取61-66
+const getPackageData = (arr: Array<{ [key in string]: any }>) => {
+    // @ts-ignore 
+    const start = arr.findIndex(val => val.__rowNum__ === 59)
+    // @ts-ignore eslint-disable-next-line
+    const end = arr.findIndex(val => val.__rowNum__ === 67)
+
+    return arr.slice(start + 1, end).map(val => {
+        return { "Packaging": val['__EMPTY_1'], "Packaging Name":val.__EMPTY_2,"Yearly need":val.__EMPTY_3}
+    })
+}
+
+// 获取76-85
+const getData2 = (arr: Array<{ [key in string]: any }>) => {
+    // @ts-ignore 
+    const start = arr.findIndex(val => val.__rowNum__ === 74)
+    // @ts-ignore eslint-disable-next-line
+    const end = arr.findIndex(val => val.__rowNum__ === 86)
+
+    const table1 = arr.slice(start + 1, end).map(val => {
+        return { 
+            "Packaging": val['UD-KMP'], 
+            "weekly need":val.__EMPTY,
+            "Packaging Name": val['__EMPTY_4'],
+            "Yearly need":val['__EMPTY_5'],
+        }
+    })
+    // const table2 = arr.slice(start + 1, end).map(val => {
+    //     return { 
+    //         "Packaging": val['__EMPTY_3'],
+    //         "Packaging Name": val['__EMPTY_4'],
+    //         "Yearly need":val['__EMPTY_5'],
+    //     }
+    // })
+    // return [table1, table2]
+    return [table1]
+    
+}
+
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const validate = (json: any) => {
     if (!json['Supplier parma code']) return 'Please input parma code'
@@ -104,6 +145,7 @@ export default memo(function App() {
     const [error, setError] = useState('');
     const [parmainfo, setParmainfo] = useState<JsonData>();
     const [address, setAddress] = useState([]);
+    const [showBtn, setShowBtn] = useState(false)
     // const [apiResponse, setApiResponse] = useState<any>(null);
 
     // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
@@ -123,7 +165,7 @@ export default memo(function App() {
                     console.log("Json", jsonData)
 
                     const json = {
-                        'Supplier parma code': getArrayKey(jsonData, 'Supplier parma code :')?.__EMPTY_2
+                        'Supplier parma code': getArrayKey(jsonData, 'Supplier parma code :')?.__EMPTY_1
                     }
 
                     setData(json)
@@ -145,10 +187,17 @@ export default memo(function App() {
                         }).then(res => res.json()).then(res => {
                             console.log(res);
                             setParmainfo(res);
+                            setShowBtn(true)
                         }).catch(error => {
                             console.log(error)
                         })
+                    } else {
+                        setShowBtn(true)
                     }
+
+                    
+            console.log("61-66", getPackageData(jsonData))
+            console.log("77-86两张表", getData2(jsonData))
                 }
             };
             reader.readAsBinaryString(file);
@@ -177,7 +226,7 @@ export default memo(function App() {
             // 获取不固定子表后面的数据
             const secItems = items.slice(index);
             console.log(secItems)
-            console.log('Packaging account no', secItems[3].__EMPTY_2)
+            console.log('Packaging account no', secItems[3].__EMPTY_1)
         }
     }, [parmainfo, address])
 
@@ -192,53 +241,53 @@ export default memo(function App() {
         // 获取不固定子表后面的数据
         const secItems = items.slice(index);
         console.log(secItems)
-        console.log('Packaging account no', secItems[3].__EMPTY_2)
+        console.log('Packaging account no', secItems[3].__EMPTY_1)
         const request = {
-            PARMANo: String(items[3]?.__EMPTY_2),
-            CompanyName: items[4]?.__EMPTY_2,
+            PARMANo: String(items[3]?.__EMPTY_1),
+            CompanyName: items[4]?.__EMPTY_1,
             ASNStreet: address[0]?.street,      //String(items[6]?.__EMPTY_2),
             ASNPostCode: String(address[0]?.postalCode),//String(items[7]?.__EMPTY_2),
             ASNCountryCode: String(address[0]?.countryCode),//String(items[8]?.__EMPTY_2),
             ASNPhone: String(address[0]?.phoneNumber),//String(items[9]?.__EMPTY_2),
             //Title:"111"
-            BilltoNo: String(items[11]?.__EMPTY_2),
+            BilltoNo: String(items[11]?.__EMPTY_1),
             BillStreet: address[1]?.street,//String(items[12]?.__EMPTY_2),
             BillPostCode: String(address[1]?.postalCode),//String(items[13]?.__EMPTY_2),
             BillCountryCode: String(address[1]?.countryCode),//String(items[14]?.__EMPTY_2),
             BillPhone: String(address[0]?.phoneNumber),//String(items[15]?.__EMPTY_2),
             //
-            ShipToNo: String(items[17]?.__EMPTY_2),
+            ShipToNo: String(items[17]?.__EMPTY_1),
             ShipStreet: address[1]?.street,//String(items[18]?.__EMPTY_2),
             ShipPostcode: String(address[2]?.postalCode),//String(items[19]?.__EMPTY_2),
             ShipCountryCode: String(address[2]?.countryCode),//String(items[20]?.__EMPTY_2),
             ShipPhone: String(address[2]?.phoneNumber),//String(items[21]?.__EMPTY_2),
             //
-            VatNo: String(getArrayKey(items, 'VAT No:')?.__EMPTY_2),//String(items[22]?.__EMPTY_2),
-            GSDBID: String(getArrayKey(items, 'GSDB ID:')?.__EMPTY_2),//String(items[23]?.__EMPTY_2),
+            VatNo: String(getArrayKey(items, 'VAT No:')?.__EMPTY_1),//String(items[22]?.__EMPTY_2),
+            GSDBID: String(getArrayKey(items, 'GSDB ID:')?.__EMPTY_1),//String(items[23]?.__EMPTY_2),
             //
-            ContractName: String(getArrayKey(items, 'First and Last Name:')?.__EMPTY_2),//String(items[23]?.__EMPTY_2),
-            ContractEmail: String(getArrayKey(items, 'E-mail:')?.__EMPTY_2),//String(items[25]?.__EMPTY_2),
-            ContractPhoneno: String(getArrayKey(items, 'Phone No:')?.__EMPTY_2),// String(items[26]?.__EMPTY_2),
+            ContractName: String(getArrayKey(items, 'First and Last Name:')?.__EMPTY_1),//String(items[23]?.__EMPTY_2),
+            ContractEmail: String(getArrayKey(items, 'E-mail:')?.__EMPTY_1),//String(items[25]?.__EMPTY_2),
+            ContractPhoneno: String(getArrayKey(items, 'Phone No:')?.__EMPTY_1),// String(items[26]?.__EMPTY_2),
             //
             ReceivingJSON:JSON.stringify(getSubTableData(items)),
             //
-            Constatus:secItems[3]?.__EMPTY_2 !==null? "Yes":"No",
-            ConPackagingAccno:String(secItems[3]?.__EMPTY_2),
-            ConCompanyName:String(secItems[4]?.__EMPTY_2),
-            ConCity:String(secItems[5]?.__EMPTY_2),
-            ConCountryCode:String(secItems[6]?.__EMPTY_2),
+            Constatus:secItems[0]?.__EMPTY_1 !=="Y"? "Yes":"No",
+            ConPackagingAccno:String(secItems[2]?.__EMPTY_1),
+            ConCompanyName:String(secItems[3]?.__EMPTY_1),
+            ConCity:String(secItems[4]?.__EMPTY_1),
+            ConCountryCode:String(secItems[5]?.__EMPTY_1),
             //
             // 需要写一个JSON 存住 Excel 62-67 行 类似于ReceivingJSON:JSON.stringify(getSubTableData(items)),
-            //ConsequensesJSON:JSON.stringify()
+            ConsequensesJSON:JSON.stringify(getPackageData(items)),
             // 还有一个JSON 存住Excel 77-82 
-            //PackageJSON:JSON.stringify()
+            PackageJSON:JSON.stringify(getData2(items)),
             
             //这个日期 要日月年, 看看怎么做 这个存的时候要日期格式 
-            // RequestDate:String(secItems[26]?.__EMPTY_2).
-            IssuCompName:String(secItems[28]?.__EMPTY_2),
-            IssuName:String(secItems[29]?.__EMPTY_2),
-            IssuPhoneNo:String(secItems[30]?.__EMPTY_2),
-            IssuEmail:String(secItems[31]?.__EMPTY_2),
+            RequestDate:moment(String(secItems[29]?.__EMPTY_1),"dd-MM-YYYY"),
+            IssuCompName:String(secItems[31]?.__EMPTY_1),
+            IssuName:String(secItems[32]?.__EMPTY_1),
+            IssuPhoneNo:String(secItems[33]?.__EMPTY_1),
+            IssuEmail:String(secItems[34]?.__EMPTY_1),
 
 
         }
@@ -269,7 +318,9 @@ export default memo(function App() {
                             error && <Alert message={error} type="error" />
                         }
                     </Space>
-                    <Button style={{ width: 150 }} onClick={() => submitFunction()}>Submit</Button>
+                    {
+                        showBtn && <Button style={{ width: 150 }} onClick={() => submitFunction()}>Submit</Button>
+                    }
                 </Stack>
 
             </div>
