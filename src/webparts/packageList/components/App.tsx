09cf7308b58536a8c15fb21e8f1209134e684360
Rodger Ruan <rodger.ruan@udtrucks.se>
2024-02-04 16:54:02 +0800

    package list basic structure added

--- src/webparts/packageList/components/App.tsx
+++ src/webparts/packageList/components/App.tsx
@@ -1,11 +1,75 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { memo } from "react";
 import * as React from "react";
+import { ArrowLeftOutlined } from "@ant-design/icons";
 import PackageListView from "./packagelistview";
+import "./App.css";
+import styles from "./PackageList.module.scss";
+import { usePackagings } from "../../../common/hooks/usePackagings";
+import { Alert, Col, Divider, Row, Spin } from "antd";
+import AppContext from "../../../common/AppContext";
+import { PackagingStatus } from "../../../common/features/packagings/packagingSlice";
 export default memo(function App() {
+  const [isFetching, errorMessage, packagingNeeds, fetchPackagingNeeds] =
+    usePackagings();
+  const [initial, setInitial] = React.useState(false);
+  const appContext = React.useContext(AppContext);
+  const isLoadingCase = isFetching === PackagingStatus.Loading;
+  React.useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    async function waitForData() {
+      setInitial(true);
+      // eslint-disable-next-line no-constant-condition
+      while (true) {
+        fetchPackagingNeeds();
+        break;
+      }
+      setInitial(false);
+    }
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    waitForData().catch(console.error);
+  }, []);
   return (
     <>
-      <PackageListView />
+      <div className={styles.listWrapper}>
+        <Row align="middle">
+          <Col offset={2}>
+            <a
+              href={`${appContext.context.pageContext.web.absoluteUrl}/SitePages/Home.aspx`}
+              style={{ color: "#00829B" }}
+            >
+              <ArrowLeftOutlined rev={undefined} />
+              <span>Return to home</span>
+            </a>
+          </Col>
+        </Row>
+        <Row>
+          <Col offset={2} span={20}>
+            {(isLoadingCase || initial) && (
+              <Spin tip="Loading...">
+                <Alert
+                  className={styles.alertStyle}
+                  message=""
+                  description=""
+                  type="info"
+                />
+              </Spin>
+            )}
+            {errorMessage?.length !== 0 && !initial && (
+              <Alert
+                message="Error"
+                description={errorMessage}
+                type="error"
+                showIcon
+              />
+            )}
+            {errorMessage?.length === 0 && !isLoadingCase && !initial && (
+              <PackageListView />
+            )}
+          </Col>
+        </Row>
+        <Divider />
+      </div>
     </>
   );
 });
--- src/webparts/packageList/components/packagelistview/index.tsx
+++ src/webparts/packageList/components/packagelistview/index.tsx
@@ -2,46 +2,67 @@
 import * as React from "react";
 import "antd/dist/antd.css";
 import styles from "../PackageList.module.scss";
-import { Button, Col, Row } from "antd";
+import { Button, Card, Col, Input, Row } from "antd";
 import { usePackagings } from "../../../../common/hooks/usePackagings";
-import groupBy from "lodash/groupBy";
 
 const PackageListView: React.FC = () => {
   //#region interfaces
   //#endregion
   //#region fields
-  const [isFetching, errorMessage, packagingNeeds, fetchPackagingNeeds] =
-    usePackagings();
+  const [, , packagingNeeds] = usePackagings();
   const [currentPackagingNeeds, setCurrentPackagingNeeds] =
     React.useState(packagingNeeds);
-
   //#endregion
   //#region methods
-  const SearchPackagings = async (): Promise<void> => {
-    fetchPackagingNeeds();
-    return;
-  };
+  React.useEffect(() => {
+    setCurrentPackagingNeeds(packagingNeeds);
+  });
   //#endregion
   //#region events
-  const onSearchPackagings = async (): Promise<void> => {
-    await SearchPackagings().then(() => {
-      setCurrentPackagingNeeds(packagingNeeds);
-    });
-    return;
-  };
   //#endregion
   //#region components
-
   //#endregion
   return (
-    <div className={styles.listWrapper}>
-      <Row>
-        <Col>
-          <Button onClick={onSearchPackagings}>Search</Button>
+    <div>
+      <Row className={styles.title}>
+        <Col>Case Handling</Col>
+      </Row>
+      <Row className={styles.rowContent}>
+        <Col span={24}>
+          <Card bordered={false}>
+            <Row className={styles.rowContent} align="middle">
+              <Col span={4}>Parma</Col>
+              <Col span={4}>
+                <Input />
+              </Col>
+              <Col offset={1} span={5}>
+                Supplier Name
+              </Col>
+              <Col span={5}>
+                <Input />
+              </Col>
+            </Row>
+            <Row className={styles.rowContent} align="middle">
+              <Row className={styles.rowContent} align="middle">
+                <Col span={4}>Year</Col>
+                <Col span={4}>
+                  <Input />
+                </Col>
+                <Col offset={1} span={5}>
+                  Case ID
+                </Col>
+                <Col span={7}>
+                  <Input />
+                </Col>
+              </Row>
+            </Row>
+          </Card>
         </Col>
       </Row>
-      <Row>
-        <Col span={24}>{"[data area]"}</Col>
+      <Row className={styles.sectionTwo}>
+        <Col span={24}>
+          <Card bordered={false}></Card>
+        </Col>
       </Row>
     </div>
   );
